a59238b14581e16ba86ee85d63e5332b8271d333
Rosenberg Xiangyu Liu <rosenbergliu@gmail.com>
2024-07-09 00:02:04 +1000

    Create project and configure tailwind css

---
+++ ToDoList.Domain/ToDoState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList.Domain
+{
+    public enum ToDoState
+    {
+        Unknown = 0,
+        Created = 1,
+        Finished = 2,
+        Overdue = 3,
+        Deleted = 4
+    }
+}
